8c08e60a96cda631e0fb6483a0dc3a3ec4d4a199
deyeni2001 <deyeni2001@yahoo.com>
2022-01-09 11:27:11 +0100

    Final commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,31 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
 
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+
+  useEffect(() => {
+    setLoading(true);
+    fetch("https://course-api.com/react-tours-project")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const tours = [];
+        for (const key in data) {
+          const tour = {
+            id: key,
+            ...data[key],
+          };
+          tours.push(tour);
+        }
+        setLoading(false);
+        setTours(tours);
+      });
+  }, []);
+
   if (loading) {
     return (
       <main>
@@ -14,9 +39,18 @@ function App() {
       </main>
     );
   }
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>No tours left</h2>
+        </div>
+      </main>
+    );
+  }
   return (
     <main>
-      <Tours />
+      <Tours tours={tours} removeTour={removeTour} />
     </main>
   );
 }
--- src/Tour.js
+++ src/Tour.js
@@ -1,7 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+const Tour = (props) => {
+
+  const [readMore,setReadMore] = useState(false);
+  return (
+    <article className="single-tour">
+      <img src={props.image} alt={props.name} />
+      <footer>
+        <div className="tour-info">
+          <h4>{props.name}</h4>
+          <h4 className="tour-price">${props.price}</h4>
+        </div>
+        <p>
+          {readMore ? props.info : `${props.info.substring(0,250)}...`}
+          <button onClick={()=> setReadMore(!readMore)}>
+           {readMore? 'show less':'read more'} 
+            </button>
+          </p>
+          <button className="delete-btn" onClick={()=> props.removeTour(props.id)}>Not interested</button>
+      </footer>
+    </article>
+  );
 };
 
 export default Tour;
